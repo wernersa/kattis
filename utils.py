d70aa257c238ec0772147839b45d0291f7f9a843
Werner Sævland <werner.saevland@gmail.com>
2019-03-04 09:38:40 +0100

    Add argument parser

--- utils.py
+++ utils.py
@@ -9,11 +9,6 @@ from urllib.request import urlopen, Request
 
 SOLUTIONS = 'sample_solutions'
 
-# TODO:
-# parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument('integers', metavar='N', type=str, nargs='+',
-#                     help='an integer for the accumulator')
-
 # Check if the sample solutions directory exists
 if not os.path.isdir(SOLUTIONS):
     os.makedirs(SOLUTIONS)
@@ -33,6 +28,7 @@ class Kattis_Problem(object):
             os.makedirs(self.folder)
         elif len(os.listdir(self.folder)) > 0:
             # Directory is not empty
+            print("Solution samples already downloaded")
             return
         
         url = f'https://open.kattis.com/problems/{self.id}/file/statement/samples.zip'
@@ -49,17 +45,18 @@ class Kattis_Problem(object):
         names = set([self.folder + filename.split('.')[-2] for filename in os.listdir(self.folder)])
         for filename in sorted(names):
             
+            file_in = open(filename + '.in')
             out = []
             with subprocess.Popen(["python", f"{self.id}.py"], stdin=file_in, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+                #catch stderr?
                 for line in p.stdout:
-                    out.append(line)
-                    print(line, end='') # process line here
+                    out.append(line.rstrip('\n'))
+                    print(line, end='')
 
             if p.returncode != 0:
                 raise subprocess.CalledProcessError(p.returncode, p.args)
 
             # Input
-            # file_in = open(filename + '.in')
             # out = subprocess.run(["python", f"{self.id}.py"], stdin=file_in, stdout=subprocess.PIPE)
             # file_in.close()
             # out = out.stdout.decode("utf-8").splitlines()
@@ -75,12 +72,15 @@ class Kattis_Problem(object):
 
 # For testing purposes. Will only run if this file is executed:
 if __name__ == "__main__":
-    # If running the file directly, it is a test case:
-    problem_id = "different"
-    
-    if sys.argv[1] is not None:
-        problem_id = sys.argv[1]
 
-    problem = Kattis_Problem("different")
+    parser = argparse.ArgumentParser(description='Utility functions for solving problems on kattis')
+    parser.add_argument('id',help='The problem id found on kattis', nargs='?', default="different")
+    args = vars(parser.parse_args())
+
+    if args['id']:
+        problem_id = args['id']
+        # Test using "different" if not argument is used
+    
+    problem = Kattis_Problem(problem_id)
     problem.download()
     problem.solve()
