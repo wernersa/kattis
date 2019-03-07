468baad01156ea8167228f63fcd11d6db88010a6
Werner Sævland <werner.saevland@gmail.com>
2019-03-07 12:50:37 +0100

    Add Problem: modulo

--- modulo.py
+++ modulo.py
@@ -1,3 +1,9 @@
 import sys
+W = [int(k) for k in sys.stdin.readlines()]
+res = set()
 
-W, P = [int(k) for k in sys.stdin.readline().split(" ")]
+for num in W:
+    mod = num % 42
+    res.add(mod)
+
+print(len(res))
