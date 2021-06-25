62dc0b1b97449a1fbb63a120d24244551bdd99e5
Werner Sævland <werner.saevland@gmail.com>
2021-06-25 23:14:24 +0200

    Add problem 'ballotboxes'

--- problems/ballotboxes.py
+++ problems/ballotboxes.py
@@ -1,8 +1,10 @@
+#TODO: NOT SOLVED
+
 import sys
 import math
 
 for case in range(3):
-    N, B = [int(x) for x in sys.stdin.readline().strip("\n").split(" ")]
+    N, B = [int(x) for x in sys.stdin.readline().split()]
     if N < 0:
         # All cases done, finished
         break
@@ -11,24 +13,24 @@ for case in range(3):
     # One ballot box for each city
     B = B - N
 
+    cities_sum = 0
     for c in range(N):
-        # [ population, ballotbox, assigned_population]
-        population = int(sys.stdin.readline().strip("\n"))
+        # [ population_per_box, ballotbox, population]
+        population = int(sys.stdin.readline().strip())
         cities.append([population, 1, population])
+        cities_sum += population
     
-    cities.sort(key=lambda x: x[2]) #Sort by assigned_population
-    cities_subset = cities[-(B + 1):] #Subset a minimum 
+    cities.sort(reverse=True)
+    
+    cities_proporitons = [population[0] / cities_sum for population in cities]
+
+    cities_subset = cities[:(B + 1)]  # Subset a minimum
     while B > 0:
-        pot_boxes = 1
-        cities_subset[-1][1] += pot_boxes
-        cities_subset[-1][2] = cities_subset[-1][0] / cities_subset[-1][1]
-        B -= pot_boxes
-        cities_subset.sort(key=lambda x: x[2]) #Sort by assigned_population
-        cities_subset = cities_subset[-(B + 1):] #Subset a minimum
-    print(int(math.ceil(cities_subset[-1][2])))
+        cities_subset[0][1] += 1  # Increment assigned boxes by one
+        B -= 1  # Remove one box from the total pool available
+        cities_subset[0][0] = cities_subset[0][2] / cities_subset[0][1]  # Calculate the assigned population per box
+        cities_subset.sort(reverse=True)  # Sort by assigned_population
+    print(int(math.ceil(cities_subset[0][0])))
 
+    # Skip one line between each case
     sys.stdin.readline()
-
-# print("output")
-
-#print("error", file=sys.stderr )
